d4fba59f36286442171c8c8ce2a9e64eaf87c1af
Divyanshu Rawat <divyanshu.19r@gmail.com>
2021-03-28 00:35:01 +0530

    Day - 11 Commit

---
+++ Day 11 - 27 March 2021/Diameter of Binary Tree.cpp
@@ -0,0 +1,31 @@
+// Problem Link : https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = -1;
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        height(root);
+        return res;        
+    }
+    
+    int height(TreeNode* root) {
+        if(!root) return 0;
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        res = max(res,lh+rh);
+        return 1+max(lh,rh);        
+    }
+};
